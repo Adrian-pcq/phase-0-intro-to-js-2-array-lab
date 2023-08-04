4228408bbec14465b7c7d9be6c75e52d55a5bd7d
Adrian <adrianqueizan@gmail.com>
2023-08-03 23:07:27 -0400

    creates arrays

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(){
+  const appendCat = [...cats, "Broom"];
+return appendCat
+}
+
+function prependCat(){
+    const prependcat = ["Arnold", ...cats];
+    return prependcat
+}
+
+function removeLastCat(){
+    const remove = cats.slice(0,2)
+    return remove
+}
+
+function removeFirstCat(){
+    const remove = cats.slice(1,3)
+    return remove
+}
